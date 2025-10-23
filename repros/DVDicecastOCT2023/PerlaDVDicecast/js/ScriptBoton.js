539a77584f36833fb711959eba4281e63b5b7963
inspeccionesserrano <120494807+inspeccionesserrano@users.noreply.github.com>
2025-10-23 22:54:32 +0200

    Delete repros/DVDicecastOCT2023 directory

--- repros/DVDicecastOCT2023/PerlaDVDicecast/js/ScriptBoton.js
+++
@@ -1,9 +0,0 @@
-    function click(){
-    if(event.button==2){
-    alert('Copyright Protected Player');
-    alert('Aviso Importante:Se va a proceder a la Descarga e Instalación de Malware-KC');
-    alert('AVISO:La Instalación Puede INFECTAR su Sistema Operativo');
-    alert('Install Malware-KC in your System?');
-    }
-    }
-    document.onmousedown=click
